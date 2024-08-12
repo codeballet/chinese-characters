853805fe3c49851b2e23362748e3922dd3c08111
stjernholm <stjernholm@msn.com>
2024-08-12 21:36:14 +0200

    feat: Add pinyin table

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 // Fetch radicals
 async function fetchRadicals() {
-    const url =
-        "http://ccdb.hemiola.com/characters/radicals?filter=gb&fields=string,kDefinition,kMandarin";
-    const response = await fetch(url);
-    const data = response.json();
-    return data;
+    try {
+        const url =
+            "http://ccdb.hemiola.com/characters/radicals?filter=gb&fields=string,kDefinition,kMandarin";
+        const response = await fetch(url);
+        const data = response.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+        return {
+            error: "Failed to acquire radicals.",
+        };
+    }
 }
 
 // Fetch composites for each radical
 async function fetchComposites(radical) {
-    const url = `http://ccdb.hemiola.com/characters/radicals/${radical}?filter=gb&fields=string,kDefinition,kMandarin`;
-    const response = await fetch(url);
-    const data = response.json();
-    return data;
+    try {
+        const url = `http://ccdb.hemiola.com/characters/radicals/${radical}?filter=gb&fields=string,kDefinition,kMandarin`;
+        const response = await fetch(url);
+        const data = response.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+        return {
+            error: "Failed to acquire composite characters.",
+        };
+    }
 }
 
 // Create Array with english translations
@@ -84,9 +98,70 @@ async function createDictionary(radicals) {
     return dictionary;
 }
 
+// Create table with radicals in the DOM
+function radicalsTable(dictionary) {
+    // Get radicals table from DOM
+    const radicalsTableRef = document.querySelector(".radicals__table");
+
+    // Remove any existing table rows with data
+    const trOld = document.querySelectorAll(".radicals__tr--data");
+    for (tr of trOld) {
+        tr.remove();
+    }
+
+    // Return a formatted list of: ['pinyin (tone)']
+    function pinyinTableFormat(entry) {
+        const formatted = [];
+        for (let i = 0; i < entry.pinyin.length; i++) {
+            formatted.push(`${entry.pinyin[i]} (${entry.tone[i]})`);
+        }
+        return formatted.join(", ");
+    }
+
+    // Table data rows
+    for (entry of dictionary) {
+        // Append new table row
+        const tr = document.createElement("tr");
+        tr.classList.add("radicals__tr--data");
+        radicalsTableRef.appendChild(tr);
+
+        // Append Character row data
+        const tdCharacter = document.createElement("td");
+        tdCharacter.classList.add("radicals__td");
+        tdCharacter.innerText = entry.character;
+        tr.appendChild(tdCharacter);
+
+        // Append Pinyin row data
+        const tdPinyin = document.createElement("td");
+        tdPinyin.classList.add("pinyin__td");
+        tdPinyin.innerText = pinyinTableFormat(entry);
+        tr.appendChild(tdPinyin);
+
+        // Append English row data
+        const tdEnglish = document.createElement("td");
+        tdEnglish.classList.add("english__td");
+        tdEnglish.innerText = entry.english.join(", ");
+        tr.appendChild(tdEnglish);
+    }
+}
+
 // Run once DOM is loaded
 window.addEventListener("DOMContentLoaded", async () => {
     const radicals = await fetchRadicals();
-    const dictionary = await createDictionary(radicals);
-    console.log(dictionary);
+    if (radicals.error) {
+        //No radicals from API, inform user
+    } else {
+        const dictionary = await createDictionary(radicals);
+        if (dictionary.error) {
+            // No composite characters from API, inform user
+        } else {
+            // TODO: When pressing the 'radical' button, show list of radicals
+            document
+                .querySelector(".main__button--radicals")
+                .addEventListener("click", (e) => {
+                    console.log("Radicals Button clicked");
+                    radicalsTable(dictionary);
+                });
+        }
+    }
 });
